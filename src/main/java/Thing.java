49f89d0164adfabf79f709d367493dd14b4d894f
snaxxy <snaxxy72@gmail.com>
2017-03-01 20:35:55 +0300

    Initial commit

---
+++ src/main/java/Thing.java
@@ -0,0 +1,68 @@
+import java.util.Random;
+
+/*
+Класс элементов
+ */
+public class Thing {
+
+    final Random random = new Random();
+    private int weight;
+    private int volume;
+    private int ID; //идентификатор элемента
+    private int[] criterionSet=new int[5]; //массив критериев
+    private static int IDs=0; //счетчик идентификаторов элементов
+    private int layerSum=0;
+
+
+
+//TODO: magic numbers
+    public Thing() //конструктор
+    {
+        this.weight=random.nextInt(3)+1;
+        this.volume=random.nextInt(3)+1;
+        this.ID=IDs++;
+        for (int i=0; i<5; ++i) {
+            criterionSet[i] = random.nextInt(3)+1;
+        }
+    }
+
+    public int[] getCriterionSet()
+    {
+        return this.criterionSet;
+    }
+
+    public int getWeight()
+    {
+        return this.weight;
+    }
+
+    public int getVolume()
+    {
+        return this.volume;
+    }
+
+    public int getID()
+    {
+        return this.ID;
+    }
+
+    public int criterionSum()
+    {
+        int sum=0;
+        for (int i=0; i< this.criterionSet.length; ++i)
+        {
+            sum+=criterionSet[i];
+        }
+        return sum;
+    }
+
+    public void setLayerSum(int layerSum)
+    {
+        this.layerSum=layerSum;
+    }
+
+    public int getLayerSum()
+    {
+        return this.layerSum;
+    }
+}
